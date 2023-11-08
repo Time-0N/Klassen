42404d38a6bdf08a14132c21b5c1e9560469c7ad
Time_ON <haas.timeon@gmail.com>
2023-11-08 10:38:28 +0100

    start

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    String marke;
+    String modell;
+    String farbe;
+    int baujahr;
+    int kmh;
+
+    Car(String marke, String modell, String farbe, int baujahr, int kmh) {
+        this.marke = marke;
+        this.modell = modell;
+        this.farbe = farbe;
+        this.baujahr = baujahr;
+        this.kmh = kmh;
+
+        kmh = speedUpCar();
+    }
+    public int speedUpCar() {
+        kmh += 10;
+        return kmh;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void startFirstCar() {
+        System.out.println("Started first car");
+    }
+
+    public static void main(String[] args) {
+        Car firstCar = new Car("Mercedes", "GT Coupé", "Grey", 2014, 80);
+        startFirstCar();
+
+        System.out.println("Speed of first car: " + firstCar.kmh);
+
+        Car secondCar = new Car("Toyota", "Corolla", "Rot", 2023, 70);
+
+
+        System.out.println("Speed of second car: " + secondCar.kmh);
+
+    }
 }
